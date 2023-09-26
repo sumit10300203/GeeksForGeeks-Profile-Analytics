4f9fa420d8c302e1727a9ed1267f43dee1271216
Sumit Dhar <66067910+sumit10300203@users.noreply.github.com>
2023-09-26 21:03:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 # from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.action_chains import ActionChains
