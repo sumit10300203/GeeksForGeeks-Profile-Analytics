8c2030d2510b3167ac6c4da08ad7b14b6784b459
Sumit Dhar <66067910+sumit10300203@users.noreply.github.com>
2023-09-26 21:28:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -89,12 +89,12 @@ def load_lottiefile(filepath: str):
 # options.add_experimental_option('useAutomationExtension', False)
 options = Options()
 # options.add_argument("--headless")
-# options.add_argument("--no-sandbox")
-# options.add_argument("--disable-dev-shm-usage")
-# options.add_argument("--disable-gpu")
-# options.add_argument("--disable-features=NetworkService")
-# options.add_argument("--window-size=1920x1080")
-# options.add_argument("--disable-features=VizDisplayCompositor")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-gpu")
+options.add_argument("--disable-features=NetworkService")
+options.add_argument("--window-size=1920x1080")
+options.add_argument("--disable-features=VizDisplayCompositor")
 
 def delete_selenium_log():
     if os.path.exists('selenium.log'):
