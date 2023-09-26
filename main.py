d8dcc69012ee5d649e2ed1b76dd877ef6d782aef
Sumit Dhar <66067910+sumit10300203@users.noreply.github.com>
2023-09-26 21:29:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ def load_lottiefile(filepath: str):
 # options.add_experimental_option("excludeSwitches", ["enable-automation"])
 # options.add_experimental_option('useAutomationExtension', False)
 options = Options()
-# options.add_argument("--headless")
+options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--disable-gpu")
